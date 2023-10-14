cb87f9cf2d8a3d6b76848e46a72b3944018b80c5
kumardevnotes <kumar.thoudishetti@gmail.com>
2023-10-14 09:51:20 -0500

    first commit

---
+++ src/test/java/FirstDemo.java
@@ -0,0 +1,17 @@
+
+public class FirstDemo {
+
+	public static void main(String[] args) {
+		System.out.println("Hello All!");
+		
+		int age = 30;
+		
+		if(age >= 18) { // 20 >= 18 --> true
+			System.out.println("You are eligible to Vote");
+		}
+		else
+		{
+			System.out.println("You are not eligible to Vote");
+		}
+	}
+}
